a53506a43fa3fc06065ee71a372c9a576995dc0a
ChanminLee <131941615+SUSOT@users.noreply.github.com>
2024-07-25 13:20:50 +0900

    Title: 변수 값 교체하기 6, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 2;
+    int  b = 5;
+    int temp;
+
+    temp = b;
+    b = a;
+    a = temp;
+    cout << a << endl << b;
+    return 0;
+}
