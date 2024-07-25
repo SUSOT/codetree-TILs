c8aa80bb455a3e7fe0ff0a91c90c99492e00e129
ChanminLee <131941615+SUSOT@users.noreply.github.com>
2024-07-25 13:21:59 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/데이터 교환/exchange-data.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int temp;
+
+    temp = c;
+    c = b;
+    b = a;
+    a = temp;
+    cout << a << endl << b << endl << c;
+    return 0;
+}
