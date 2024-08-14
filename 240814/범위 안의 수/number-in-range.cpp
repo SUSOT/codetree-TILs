ff16e8eb92aabe7372a3074ebc95af82e2e12ca0
ChanminLee <131941615+SUSOT@users.noreply.github.com>
2024-08-14 10:50:10 +0900

    Title: 범위 안의 수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240814/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    if(a >= 10 && a <= 20)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
