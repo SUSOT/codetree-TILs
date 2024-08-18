4721dfb0fd57c45a68250c879f729989df168fa3
ChanminLee <131941615+SUSOT@users.noreply.github.com>
2024-08-19 08:37:07 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240819/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    if(a > c)
+    {
+        cout << "A";
+    }
+    else
+    {
+        cout << "B";
+    }
+    if(a == c)
+    {
+        if(b > d)
+        {
+            cout << "A";
+        }
+        else
+        {
+            cout << "B";
+        }
+    }
+    return 0;
+}
