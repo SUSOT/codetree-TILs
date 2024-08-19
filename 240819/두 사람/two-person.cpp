b90e80340137a637ae43b6cd41dffb48c9ff417f
ChanminLee <131941615+SUSOT@users.noreply.github.com>
2024-08-19 14:59:38 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240819/두 사람/two-person.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,c
+    char b,d;
+    cin >> a >> b >> c >> d;
+    if((a >= 19 || b >= 19) && (b == 'M' || d == 'M'))
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
